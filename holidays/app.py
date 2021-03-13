a480ec0eaadd23f02bcfc1cd7e80154033c6bc61
Abdul Q <qureshi.abdul.227@gmail.com>
2021-03-13 13:48:52 -0500

    final project was started

--- holidays/app.py
+++ holidays/app.py
@@ -9,5 +9,11 @@ app = Flask(__name__)
 def index():
     
 
-if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.213')
+
+
+
+
+
+
+    if __name__ == '__main__':
+        app.run(debug=True, host='192.168.1.213')
