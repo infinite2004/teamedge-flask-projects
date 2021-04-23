58c61ad87e13b7057cd8acd080a537ec8c7ee752
Abdul Q <qureshi.abdul.227@gmail.com>
2021-04-23 17:56:20 -0400

    PROJECT IS FINISHED

--- Final project/app.py
+++ Final project/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,url_for,redirect
-from sense_hat import SenseHat
+from sense_hat import SenseHat 
+import sqlite3
 
 sense = SenseHat()
   
@@ -30,21 +31,51 @@ sense.set_pixels(rmon1)
 @app.route('/success/<name>')
 def success(name):
     sense.show_message(name)
-    return 'welcome %s' % name
-    
+    #return 'welcome %s' % name
+    return '<a href = "/all">All messages</a>'
+
+@app.route('/all')
+def all():
+    #connect to DB
+    conn = sqlite3.connect('./static/data/senseDisplay.db')
+    curs = conn.cursor()
+    users = []
+    rows = curs.execute("SELECT * from users")
+    for row in rows:
+        name = {'name': row[0]}
+        users.append(name)
+    conn.close()
+    return render_template('all.html', users = users)
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
         user = request.form['nm']
+        # name = request.form['name']
+    
+        conn = sqlite3.connect('./static/data/senseDisplay.db') 
+        curs = conn.cursor()
+        curs.execute('INSERT INTO users (name) VALUES((?))',(user,))
+        conn.commit()
+        conn.close()
+
         return redirect(url_for('success',name = user))
+       
     else:
         user = request.args.get('nm')
         return redirect(url_for('success',name = user))
-
+       
 @app.route('/', methods=['GET'])
 def submit():
     return  render_template('post.html')
+
+   
+    
+   
+
+ 
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000,host='127.0.0.1')
